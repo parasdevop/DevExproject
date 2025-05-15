92142ade94627cecab50f113bc568c578fa818ce
parasdevop <paras.verma@arisein.com>
2025-05-15 17:06:58 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,36 @@
+using Microsoft.EntityFrameworkCore;
+using StudentPortal.Web.Data;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("StudentPortal")));
+
+var app = builder.Build();
+
+//Register services
+builder.Services.AddDevExpress - Controls();
+
+app.UseDevExpressControls();
+
+app.UseStaticFiles();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+}
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
